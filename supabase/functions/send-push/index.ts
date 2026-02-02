dfc2487969b2ef448ddb33709914938a341fd34e
Alessandro Di Cairano <aleosmac@MacBook-Pro-di-Alessandro.local>
2026-02-02 22:19:02 +0100

    Fix: Rotate VAPID keys

--- supabase/functions/send-push/index.ts
+++ supabase/functions/send-push/index.ts
@@ -1,6 +1,6 @@
-import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
-import webpush from "https://esm.sh/web-push@3.4.5"
+import webpush from "https://esm.sh/web-push@3.6.7?target=deno"
 
 const SUPABASE_URL = Deno.env.get('SUPABASE_URL')!
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
